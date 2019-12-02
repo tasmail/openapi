fe4e359e21924c155c36bb78d300366827899a63
Andrii Tkachenko <tas.gma@gmail.com>
2019-12-02 11:14:04 +0100

    Port to odoo 8.0, transient commit.

--- __openerp__.py
+++ __openerp__.py
@@ -5,45 +5,31 @@
     "name": """REST API / Open API (Swagger)""",
     "summary": """Set up API and export OpenAPI specification file for integration with whatever system you need""",
     "category": "",
-    # "live_test_url": "",
     "images": [],
-    "version": "12.0.1.0.0",
+    "version": "8.0.1.0.0",
     "application": False,
 
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "support": "apps@it-projects.info",
     "website": "https://it-projects.info/team/yelizariev",
     "license": "LGPL-3",
-    # "price": 9.00,
-    # "currency": "EUR",
 
     "depends": [
-        "web_tour",
-        "web_settings_dashboard",
     ],
     "external_dependencies": {"python": ['bravado_core', 'swagger_spec_validator'], "bin": []},
     "data": [
-        "security/ir.model.access.csv",
-        "views/assets.xml",
-        "views/openapi_view.xml",
-        "views/res_users_view.xml",
-        "views/ir_model_view.xml",
+        # "security/ir.model.access.csv",
+        # "views/assets.xml",
+        # "views/openapi_view.xml",
+        # "views/res_users_view.xml",
+        # "views/ir_model_view.xml",
     ],
     "demo": [
-        "views/assets_demo.xml",
-        "views/tour_views.xml",
-        "data/openapi_demo.xml",
+        # "views/assets_demo.xml",
+        # "data/openapi_demo.xml",
     ],
     "qweb": [
-        "static/src/xml/dashboard.xml",
-        # Сommented until we discuss it
-        # "static/src/xml/configure_api_button.xml"
     ],
-
-    "post_load": None,
-    "pre_init_hook": None,
-    "post_init_hook": None,
-    "uninstall_hook": None,
 
     "auto_install": False,
     "installable": True,
